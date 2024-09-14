c0a48bffc91460536bfb0b8e1cbf318018cc1aed
Bartosz Kostrowiecki <bartosz.kostrowiecki@gmail.com>
2024-09-14 15:22:51 +0200

    fix slugify and deslugify

--- src/domain/slug.ts
+++ src/domain/slug.ts
@@ -1,9 +1,9 @@
 export function slugify(str: string) {
-  return str.toLocaleLowerCase().replaceAll(" ", "-");
+  return encodeURIComponent(str.toLocaleLowerCase().replaceAll(" ", "-"));
 }
 
 export function unslugify<T>(str: string) {
-  const transformed = str.replaceAll("-", " ");
+  const transformed = decodeURIComponent(str.replaceAll("-", " "));
 
   return (transformed[0].toLocaleUpperCase() + transformed.substring(1)) as T;
 }
